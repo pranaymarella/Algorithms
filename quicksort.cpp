fc1febf1e641102e567fb8bd5a9059c5aa893fdb
Pranay Marella <pranaylearn@gmail.com>
2017-08-04 01:55:31 -0500

    Randomized QuickSort Algorithm with Time Complexity O(n*log(n))

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,47 +1,45 @@
 #include <iostream>
-#include <cstdlib>
 #include <vector>
+#include <cstdlib>
 
 using std::vector;
 using std::swap;
 
 int partition(vector<int> &a, int l, int r) {
-    int x = a[l];
-    int j = l;
-    for (int i = l+1; i <= r; i++) {
-        if (a[i] <= x) {
-            j++;
-            swap(a[i], a[j]);
-        }
+  int x = a[l];
+  int j = l;
+  for (int i = l + 1; i <= r; i++) {
+    if (a[i] <= x) {
+      j++;
+      swap(a[i], a[j]);
     }
-    swap(a[l], a[j]);
-
-    return j;
+  }
+  swap(a[l], a[j]);
+  return j;
 }
 
-void QuickSort(vector<int> &a, int l, int r) {
-    if (l >= r)
-        return;
+void randomized_quick_sort(vector<int> &a, int l, int r) {
+  if (l >= r) {
+    return;
+  }
 
-    int k = l + rand() % (r - l + 1);
-    swap(a[l], a[k]);
-    int m = partition(a, l, r);
+  int k = l + rand() % (r - l + 1);
+  swap(a[l], a[k]);
+  int m = partition(a, l, r);
 
-    QuickSort(a, l, m - 1);
-    QuickSort(a, m + 1, r);
+  randomized_quick_sort(a, l, m - 1);
+  randomized_quick_sort(a, m + 1, r);
 }
 
 int main() {
-    int n;
-    std::cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++)
-        std::cin >> a[i];
-
-    QuickSort(a, 0, a.size() - 1);
-
-    std::cout << "Answer: " << std::endl;
-    for (int j = 0; j < a.size(); j++)
-        std::cout << a[j] << ' ';
-    std::cout << '\n';
+  int n;
+  std::cin >> n;
+  vector<int> a(n);
+  for (size_t i = 0; i < a.size(); ++i) {
+    std::cin >> a[i];
+  }
+  randomized_quick_sort(a, 0, a.size() - 1);
+  for (size_t i = 0; i < a.size(); ++i) {
+    std::cout << a[i] << ' ';
+  }
 }
